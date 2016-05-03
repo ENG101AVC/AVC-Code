ebaeddacc615f56c61ea0419944ffbbfd855e1de
Jacob Beal <bealjaco@greta-pt.ecs.vuw.ac.nz>
2016-05-03 12:16:28 +1200

    refactoring

--- code/main.cpp
+++ code/main.cpp
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <time.h>
-extern "C" int InitHardware();
-extern "C" int WriteDigital(int chan, char level);
-extern "C" int Sleep(int sec, int usec);
-extern "C" int ReadAnalog(int ch_adc);
+extern "C" char get_pixel(int row,int col,int colour);
+extern "C" int init(int d_lev);
+extern "C" int set_motor(int motor , int speed );
+extern "C" int Sleep( int sec , int usec );
+extern "C" int take_picture();
 
 int main(){
-    InitHardware();
-}
-
-int forward(int time){
+    init(0);
+    set_motor(1, 255);
+    set_motor(2, 255);
+    Sleep(1,0);
+    set_motor(1,0);
+    set_motor(2,0);
     
+//    //For following the line.  First thoughts from Lecture 20/04/16
+//    take_picture();
+//    int sum = 0;
+//    int i;
+//    for (i=-160, i<160, i++){
+//        w = get_pixel(120, i, 3);
+//        if(w<127){
+//            w = 0;
+//        }
+//        else if(w>=127){
+//            w = 255;
+//        }
+//        sum = sum+i*w;
+//    }
 }
