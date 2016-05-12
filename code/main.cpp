315e59ece042898689d165d4f119e01f22b5f885
rhazsolomon <solomon.rhaz@gmail.com>
2016-05-12 13:43:21 +1200

    double to char
    Rhaz Mitchell

--- code/main.cpp
+++ code/main.cpp
@@ -37,7 +37,7 @@ void follow_the_line(){
     while(testClock<1){
         take_picture();
         double sum_of_pixels = 0;  
-        double specific_pixel= 0; 
+        char specific_pixel= 0; 
  
         for (int iteration=-160; iteration < 160; iteration++){
             specific_pixel = get_pixel(iteration+160, 120, 3);
