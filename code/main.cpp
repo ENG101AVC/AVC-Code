7573709ace2e61508a425a187fb384883a296ce4
MitchellHendrikse <mhendrikse123@gmail.com>
2016-05-17 11:38:24 +1200

    error fixs

--- code/main.cpp
+++ code/main.cpp
@@ -34,14 +34,15 @@ void network() {
 
 void follow_the_line(){
     int testClock = 0;
+    int max_val = 0;
+    int min_val = 255;
+    
     while(testClock<1){
-        
         
         take_picture();
         double sum_of_pixels = 0;  
         int specific_pixel= 0; 
-        int max_val = 0;
-        int min_val = 255;
+        
         for(int i = 0; i < 320; i++){
             if(get_pixel(i, 120, 3) > max_val){
                 max_val = get_pixel(i, 120, 3);
@@ -50,7 +51,8 @@ void follow_the_line(){
                 min_val = get_pixel(i, 120, 3);
             }
         }
-        printf("%d\n", max_val)
+        printf("%d\n", max_val);
+        
         for (int iteration=-160; iteration < 160; iteration++){
             specific_pixel = get_pixel(iteration+160, 120, 3);
             printf(" %d ",specific_pixel);
