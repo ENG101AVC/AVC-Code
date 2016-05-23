8463551ea94b8843b63fffa02a3ff03d185b77f0
rhazsolomon <solomon.rhaz@gmail.com>
2016-05-24 11:39:54 +1200

    Back to follow Line

--- code/main2.cpp
+++ code/main2.cpp
@@ -157,7 +157,7 @@ int main(){
 
 	//network();			// Open Gate
 
-//	follow_the_line();	// Begin Following Line
+	follow_the_line();	// Begin Following Line
 
 //	maze_navigation();	// Navigate the maze.
 
