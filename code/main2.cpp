167da2ca2a7f712ce42e71c437e3e08fd121118b
McManaway1 <andrew.mcmanaway@gmail.com>
2016-05-26 12:47:17 +1200

    Created a new Turn Code.
    Should 180 at dead zones, and pass short spots

--- code/main2.cpp
+++ code/main2.cpp
@@ -12,7 +12,7 @@ extern "C" int connect_to_server( char server_addr[15],int port);
 extern "C" int send_to_server(char message[24]);
 extern "C" int receive_from_server(char message[24]);
 extern "C" int read_analog(int ch_adc);
-extern "C" int read_digital(int chan, int direct);
+extern "C" int read_digital(int chan);
 extern "C" int select_IO(int chan, int direct);
 extern "C" int display_picture(int delay_sec, int delay_usec);
 
@@ -90,6 +90,15 @@ void turn_Rpi(int direction){
 		set_motor(1, -30);
 		set_motor(2, 30);
 		Sleep(1, 0);
+
+		set_motor(1, 0);
+		set_motor(2, 0);
+	}
+	else if(direction == 3){
+		// Forwards
+		set_motor(1, 45);
+		set_motor(2, 45);
+		Sleep(1, 500000);
 
 		set_motor(1, 0);
 		set_motor(2, 0);
@@ -193,24 +202,7 @@ void follow_the_line(){
 
 
 
-void Complete_until_maze(){
-	/*  OLD RHAZ CODE
-	num_of_decisions = 0;
-	
-	while(num_of_decisions<4){
-		follow_the_line();
-		if(num_of_decisions > 0){
-			set_motor(2, 0);
-			set_motor(1, 100);
-			set_motor(1, 30);
-			set_motor(2, 30);
-		}
-		
-		num_of_decisions++;
-		printf("---------------- Decision Number: %d\n", num_of_decisions);
-	}
-	return;
-	*/
+/*void Complete_until_maze(){
 	
 	//Define local variables
 	int testClock = 0;							// For testing the RPi.  Can terminate movement.
@@ -308,69 +300,127 @@ void Complete_until_maze(){
 	set_motor(2, 0);
 	return;
 
-}
+}*/
 
-
-/*
-
-void maze_navigation(){
+void Complete_until_maze(){ //Experimental
 	//Define local variables
-	int testClock = 0; //For testing the RPi.  Can terminate movement.
-	
-	//For Data collection
-	int left; 
-	int right;
-	int front;
-	
-	int select_IO(0, 1);
+	int testClock = 0;							// For testing the RPi.  Can terminate movement.
+	int proportional_signal_previous;
+	int num_of_white = 0;
+	int leftpixel = 0;
+	int rightpixel = 0;
+	int frontpixel = 0;
+	int threshold = determine_average();
 
-	printf("Running Analog Code");
-	while(testClock < 1000){
-		//Sets 
-		left = read_analog(0);
-		right = read_analog(1);
-		//front = read_analog(2);
-		front = read_digital(0);
-		
-		if(front < //value)
-		{
-			if (left > 250)
-			{
-				set_motor(1, -45);
-				set_motor(2, 45);	
+	while(true){
+
+		bool seeLine = false;					// Whether or not the line can be seen.
+		int current_error = 0;
+		num_of_white = 0;
+
+		take_picture();
+
+		for(int i=0; i<320; i++){
+			int error = average_error(i);
+
+			if(error >= threshold){				// If RPi sees 'white'
+				error = 1;						// Converts to binary represenation
+				seeLine = true;					// The Line can be seen
+				num_of_white++;
 			}
-			else if(right > 350){
-				set_motor(1, 45);
-				set_motor(2, -45);	
+			else{								// If RPi sees 'black'
+				error = 0;						// Converts to binary representation
 			}
-			else{
-				set_motor(1, -80);
-				set_motor(2, -80);	
-			}
+
+			current_error = current_error + error*(i-160);
 		}
-		else
+
+		int proportional_signal = (int) (current_error/PROPORTIONAL);	//Sets proportional signal
+
+		// Print checks:
+		//printf("Current Error: %d\n", current_error);
+		//printf("Proportional Signal: %d\n", proportional_signal);
+		//printf("Number of White Pixels: %d\n", num_of_white);
+
+		//Checks Pixels at this position
+		if(average_error(80) >= threshold) 
 		{
-			set_motor(1, 80);
-			set_motor(2, 80);
+			leftpixel = 1;
+		}
+		else{
+			leftpixel = 0;
 		}
 		
-		Sleep(0,500000);
+		if(average_error(240) >= threshold) 
+		{
+			rightpixel = 1;
+		}
+		else{
+			rightpixel = 0;
+		}
 		
-		testClock++;
+		if(get_pixel(160, 10, 3) >= threshold) 
+		{
+			frontpixel = 1;
+		}
+		else{
+			frontpixel = 0;
+		}
+		
+		if(num_of_white > 160)
+		{
+			turn_Rpi(3);
+			if(leftpixel == 1 && rightpixel Sleep(0,500000);== 1 && frontpixel == 0){
+				printf("Bend: T, Turn: Left\n");
+				turn_Rpi(2);
+			}
+			else if(leftpixel == 1 && rightpixel == 1 && frontpixel == 1){
+				printf("Bend: +, Turn: None\n");
+				turn_Rpi(3);
+			}
+			else if(leftpixel == 1 && rightpixel == 0 && frontpixel == 0){
+				printf("Bend: L, Turn: Left\n");
+				turn_Rpi(2);
+			}
+			else if(leftpixel == 1 && rightpixel == 0 && frontpixel == 1){
+				printf("Bend: -|, Turn: None\n");
+				turn_Rpi(3);
+			}
+			else if(leftpixel == 0 && rightpixel == 1  && frontpixel == 0){
+				printf("Bend: L, Turn: Right\n");
+				turn_Rpi(1);
+			}
+			else if(leftpixel == 0 && rightpixel == 1  && frontpixel == 1){
+				printf("Bend: |-, Turn: None\n");
+				turn_Rpi(3);
+			}
+			
+		}
+		else if(num_of_white < 30)
+		{
+			turn_Rpi(0);
+		}
+		else if(seeLine){
+			set_motor(1, 35+proportional_signal);
+			set_motor(2, 35-proportional_signal);
+			proportional_signal_previous = proportional_signal;
+		}
+		
+		int redAvr = (get_pixel(120, 120, 0) + get_pixel(160, 120, 0) + get_pixel(200, 120, 0))/3;
+		printf("Red: %d\n",redAvr);
+		if(redAvr > 160 && read_analog(0) < 300 && read_analog(1) < 300)  //If on Red Square (Maze Detector)
+		{
+			printf("SWITCHING TO WALLED CODE, Red: %d\n",redAvr);
+			return;
+		}
+		
+	testClock++;
 	}
-	
 	set_motor(1, 0);
 	set_motor(2, 0);
 	return;
+
 }
- */
-
-
-
-
-
-
-
 
 // This is the Main() method.  Controls the holistic behaviour or the RPi.
 
