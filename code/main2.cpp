f8b9c1120aa4f279c00cd065c5bca6ec50ead39f
McManaway1 <andrew.mcmanaway@gmail.com>
2016-05-30 13:16:40 +1200

    Changed Speed

--- code/main2.cpp
+++ code/main2.cpp
@@ -224,8 +224,8 @@ void Complete_until_maze(){ //Experimental
 		}
 		
 		if(seeLine){
-			set_motor(1, 35+proportional_signal);
-			set_motor(2, 35-proportional_signal);
+			set_motor(1, 30+proportional_signal);
+			set_motor(2, 30-proportional_signal);
 			proportional_signal_previous = proportional_signal;
 		}
 		
@@ -250,7 +250,7 @@ int main(){
 	init(0);			// Initialise Hardware
 			//Assigns a place in memory for average whiteness.	int proportional_signal_previous = 0;
 
-	//network();			// Open Gate
+	network();			// Open Gate
 	//Sleep(5,0);  //Walking to Track
 	follow_the_line();
 	Complete_until_maze();    // Begin Following Line
