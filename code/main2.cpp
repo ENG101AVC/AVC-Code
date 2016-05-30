fbbe952a262cf0e8f96d16fb5dc9e6d992ab6019
McManaway1 <andrew.mcmanaway@gmail.com>
2016-05-31 09:17:19 +1200

    Trial

--- code/main2.cpp
+++ code/main2.cpp
@@ -192,15 +192,15 @@ void Complete_until_maze(){ //Experimental
 			
 			if(leftpixel == 1 && rightpixel == 1 && num_of_white>310 && frontpixel == 0){
 				printf("Bend: T, Turn: Left\n");
-				proportional_signal = -20;
+				proportional_signal = -40;
 			}
 			else if(leftpixel == 1 && rightpixel == 0 && frontpixel == 0){
 				printf("Bend: L, Turn: Left\n");
-				proportional_signal = -20;
+				proportional_signal = -40;
 			}
 			else if(leftpixel == 0 && rightpixel == 1 && frontpixel == 0){
 				printf("Bend: L, Turn: Right\n");
-				proportional_signal = 20;
+				proportional_signal = 40;
 			}
 			else if(leftpixel == 1 && rightpixel == 0 && frontpixel == 1){
 				printf("Bend: -|, Turn: Front\n");
