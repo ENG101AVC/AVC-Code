ae26735c4007ce6e9ecb437a6e7b549b7c6b30a3
McManaway1 <andrew.mcmanaway@gmail.com>
2016-05-31 12:42:01 +1200

    ..

--- code/main2.cpp
+++ code/main2.cpp
@@ -214,8 +214,8 @@ void Complete_until_maze(){ //Experimental
 		}
 		
 		if(seeLine){
-			set_motor(1, 28+proportional_signal);
-			set_motor(2, 28-proportional_signal);
+			set_motor(1, 33+proportional_signal);
+			set_motor(2, 33-proportional_signal);
 			proportional_signal_previous = proportional_signal;
 			Sleep(0,100000);
 		}
