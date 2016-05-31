fc425b36435f3abf2773c0b8a702fd11196978bf
McManaway1 <andrew.mcmanaway@gmail.com>
2016-05-31 12:07:25 +1200

    ..

--- code/main2.cpp
+++ code/main2.cpp
@@ -240,7 +240,7 @@ int main(){
 	init(0);			// Initialise Hardware
 			//Assigns a place in memory for average whiteness.	int proportional_signal_previous = 0;
 
-	network();			// Open Gate
+	//network();			// Open Gate
 	follow_the_line();
 	Complete_until_maze();    // Begin Following Line
 
